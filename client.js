88a5efb1db562132daf9a6bd5312fa77946e6503
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-06-19 16:10:10 -0500

    class work

--- client.js
+++ client.js
@@ -6,7 +6,14 @@
  * 3. After the loop, log out the total number of
  *    hobbies
  */
-
+let hobbies = ["running", "reading", "gaming", "cooking"];
+hobbies.forEach((hobby, index) => {
+  let number = index + 1;
+  console.log(`${number}. ${hobby}`);
+  // console.log(number + '.' + ' ' + hobby);
+  // Two ways!
+});
+console.log(`Total hobbies: ${hobbies.length}`);
 
 // Example output
 // 1. swimming
@@ -20,33 +27,43 @@
  * 1. Create an array of colors as strings
  *    (include the color 'teal' at least once)
  * 2. Create a variable tealCount
- * 3. Write a loop that counts the number of times teal 
+ * 3. Write a loop that counts the number of times teal
  *    is in the array
  * 4. Output the array and number of times teal was found
  */
+let colors = ["teal", "yellow", "purple", "teal"];
+let tealCount = "teal";
 
+let counter = 0;
+
+for (let color of colors) {
+  console.log(color);
+}
+
+if (colors == tealCount) {
+  counter++;
+
+  console.log(counter);
+}
 
 // Example output
 // green, red, teal, orange, teal
 // Teal was found 2 times
-
 
 /**
  * #3 Even & Odd
  * -------------
  * 1. Create an array of numbers (at least 5 numbers)
  * 2. Create variables oddNumbers and evenNumbers (empty arrays)
- * 3. Write a loop that puts all the odd numbers in the oddNumbers 
+ * 3. Write a loop that puts all the odd numbers in the oddNumbers
  *    array and even numbers in the evenNumbers array.
  * 4. Output the original array, odd number array and even number array
  */
-
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
 // Odd 3, 7, 11
 // Even 2, 8, 4, 2
-
 
 /**
  * #4 Flipping Switches
@@ -57,11 +74,9 @@
  * 4. Output both arrays
  */
 
-
 // Example output
 // true, false, true, true
 // Toggled false, true, false, false
-
 
 /**
  * #5 (STRETCH) Remove 0's
@@ -73,11 +88,9 @@
  * 3. Output the array
  */
 
-
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
 // After loop 3, 0, 2, 8
-
 
 /**
  * #6 (STRETCH) Greatest Position Distance
@@ -90,7 +103,6 @@
  *    greatest position distance for that array is 3.
  * 3. Output the array and its greatest position distance
  */
-
 
 // Example output
 // 5, 1, 8, 2, 9, 1, 4, 5, 0
